25d3819dea3212c1ebf4f3416bf22eb400f48401
Jeroen Ooms <jeroenooms@gmail.com>
2016-07-04 12:51:36 +0200

    Fix Windows builds

---
+++ tools/winlibs.R
@@ -0,0 +1,8 @@
+# Build against imagemagick static website.
+if(!file.exists("../windows/imagemagick-7.0.2.2/include/ImageMagick-7/Magick++.h")){
+  if(getRversion() < "3.3.0") setInternet2()
+  download.file("https://github.com/rwinlib/imagemagick/archive/v7.0.2.2.zip", "lib.zip", quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+}
