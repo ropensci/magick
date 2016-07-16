03733a80e3a88e187c439d31e38a68e37f3d723d
Jeroen Ooms <jeroenooms@gmail.com>
2016-07-17 00:42:49 +0200

    Fix warning

--- R/onload.R
+++ R/onload.R
@@ -1,4 +1,4 @@
-.onLoad <- function(pkg, lib){
+.onLoad <- function(lib, pkg){
   if(identical("windows", .Platform$OS.type)){
     conf_path <- system.file("etc/ImageMagick-7", package = "magick")
     Sys.setenv(MAGICK_CONFIGURE_PATH = normalizePath(conf_path))
