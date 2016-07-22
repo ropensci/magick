0c82dcd118dc8f1b76961746828f7f9d596e19da
Jeroen Ooms <jeroenooms@gmail.com>
2016-07-22 12:36:35 +0200

    Add startup message

--- R/onload.R
+++ R/onload.R
@@ -3,7 +3,12 @@
     conf_path <- system.file("etc/ImageMagick-7", package = "magick")
     Sys.setenv(MAGICK_CONFIGURE_PATH = normalizePath(conf_path))
   } else if(grepl("darwin", R.Version()$os, fixed = TRUE)){
-    if(!nchar(Sys.getenv("FONTCONFIG_PATH")))
-      Sys.setenv(FONTCONFIG_PATH="/opt/X11/lib/X11/fontconfig")
+    if(!nchar(Sys.getenv("FONTCONFIG_PATH"))){
+      if(file.exists("/opt/X11/lib/X11/fontconfig")){
+        Sys.setenv(FONTCONFIG_PATH="/opt/X11/lib/X11/fontconfig")
+      } else {
+        packageStartupMessage("XQuartz not found. Fonts might not work.")
+      }
+    }
   }
 }
