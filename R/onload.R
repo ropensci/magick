1fec6475f9f59e85b0f750460460965c5fcf558e
Jeroen Ooms <jeroenooms@gmail.com>
2016-07-22 21:35:17 +0200

    Switch to ImageMagick6

--- R/onload.R
+++ R/onload.R
@@ -1,8 +1,5 @@
 .onLoad <- function(lib, pkg){
-  if(identical("windows", .Platform$OS.type)){
-    conf_path <- system.file("etc/ImageMagick-7", package = "magick")
-    Sys.setenv(MAGICK_CONFIGURE_PATH = normalizePath(conf_path))
-  } else if(autobrewed()){
+  if(autobrewed()){
     if(!nchar(Sys.getenv("FONTCONFIG_PATH"))){
       if(file.exists("/opt/X11/lib/X11/fontconfig")){
         Sys.setenv(FONTCONFIG_PATH="/opt/X11/lib/X11/fontconfig")
