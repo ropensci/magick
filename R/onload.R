14055132ebdc019b04686f176bc8729c2dad2c03
Jeroen Ooms <jeroenooms@gmail.com>
2016-07-22 13:02:51 +0200

    Good practice

--- R/onload.R
+++ R/onload.R
@@ -2,7 +2,11 @@
   if(identical("windows", .Platform$OS.type)){
     conf_path <- system.file("etc/ImageMagick-7", package = "magick")
     Sys.setenv(MAGICK_CONFIGURE_PATH = normalizePath(conf_path))
-  } else if(grepl("darwin", R.Version()$os, fixed = TRUE)){
+  }
+}
+
+.onAttach <- function(lib, pkg){
+  if(grepl("darwin", R.Version()$os, fixed = TRUE)){
     if(!nchar(Sys.getenv("FONTCONFIG_PATH"))){
       if(file.exists("/opt/X11/lib/X11/fontconfig")){
         Sys.setenv(FONTCONFIG_PATH="/opt/X11/lib/X11/fontconfig")
