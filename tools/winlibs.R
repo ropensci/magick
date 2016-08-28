5d0ae25901a5a98df6b5ee79096b074a73a6ff2c
Jeroen Ooms <jeroenooms@gmail.com>
2016-08-28 14:05:19 +0200

    Tweak Makevars.win

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,4 +1,5 @@
 # Build against imagemagick static website.
+IM_VERSION <- commandArgs(TRUE)
 if(!file.exists("../windows/imagemagick6-6.9.5-4/include/ImageMagick-6/Magick++.h")){
   if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/imagemagick6/archive/v6.9.5-4.zip", "lib.zip", quiet = TRUE)
