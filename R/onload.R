bd167a0422cbfac424dffe05b9c221daadbf452f
Jeroen <jeroen@Jeroens-MacBook-Pro.local>
2016-09-22 21:38:05 +0200

    Bundle up fonts.conf with autobrew

--- R/onload.R
+++ R/onload.R
@@ -1,9 +1,5 @@
 .onLoad <- function(lib, pkg){
   if(autobrewed()){
-    if(!nchar(Sys.getenv("FONTCONFIG_PATH"))){
-      if(file.exists("/opt/X11/lib/X11/fontconfig")){
-        Sys.setenv(FONTCONFIG_PATH="/opt/X11/lib/X11/fontconfig")
-      }
-    }
+    Sys.setenv(FONTCONFIG_PATH=file.path(lib, pkg, "etc/fonts"))
   }
 }
