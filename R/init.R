81e551b37d58ad11089751f0a25896ffa566adf9
Jeroen <jeroenooms@gmail.com>
2017-07-23 23:45:23 +0200

    Tweak startup message

--- R/init.R
+++ R/init.R
@@ -1,6 +1,6 @@
 .onAttach <- function(lib, pkg){
-  packageStartupMessage(sprintf("ImageMagick %s", as.character(magick_config()$version)))
+  packageStartupMessage(sprintf("Linkging to ImageMagick %s", as.character(magick_config()$version)))
   if(magick_config()$version >= 7)
-    warning(c("This package has been compiled with ImageMagick-7 which is known to be buggy.\n",
-"Rebuilding against ImageMagick-6 is recommended."), call. = FALSE, immediate. = TRUE)
+    packageStartupMessage(c("Attention: the 'magick' package has been compiled with ImageMagick-7 which is known to have problems.\n",
+"Rebuilding against ImageMagick-6 is recommended."))
 }
