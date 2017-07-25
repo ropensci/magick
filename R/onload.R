80e7e73127751ef4c07d2c42042883a5c4e11175
Jeroen <jeroenooms@gmail.com>
2017-07-25 23:51:11 +0200

    Tweak onload

--- R/onload.R
+++ R/onload.R
@@ -1,5 +1,10 @@
 .onLoad <- function(lib, pkg){
   if(autobrewed()){
-    Sys.setenv(FONTCONFIG_PATH=file.path(lib, pkg, "etc/fonts"))
+    fontdir <- normalizePath(file.path(lib, pkg, "etc/fonts"), mustWork = FALSE)
+    if(file.exists(fontdir)){
+      Sys.setenv(FONTCONFIG_PATH = fontdir)
+    } else if(file.exists("/opt/X11/lib/X11/fontconfig")){
+      Sys.setenv(FONTCONFIG_PATH = "/opt/X11/lib/X11/fontconfig")
+    }
   }
 }
