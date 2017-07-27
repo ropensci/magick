7e9f83553fcb5c0c960315bd46445a3d34b0a714
Jeroen <jeroenooms@gmail.com>
2017-07-27 12:29:19 +0200

    Improve performance test

--- tests/performance.R
+++ tests/performance.R
@@ -3,13 +3,12 @@
 library(ggplot2)
 
 png(tempfile(), width = 600, height = 600)
-system.time(print(qplot(mpg, wt, data = mtcars, colour = cyl)))
-dev.off()
+system.time({print(qplot(mpg, wt, data = mtcars, colour = cyl)); dev.off()})
 
 dev <- magick::image_device(600, 600, clip = FALSE)
-system.time(print(qplot(mpg, wt, data = mtcars, colour = cyl)))
-dev.off()
+system.time({print(qplot(mpg, wt, data = mtcars, colour = cyl)); dev.off()})
 
 dev <- magick::image_device(600, 600, clip = TRUE)
-system.time(print(qplot(mpg, wt, data = mtcars, colour = cyl)))
-dev.off()
+system.time({print(qplot(mpg, wt, data = mtcars, colour = cyl)); dev.off()})
+print(dev)
+
