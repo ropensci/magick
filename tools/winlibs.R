8ed4d8097e7519d58e2e64e976157a18e8b26e79
Jeroen Ooms <jeroenooms@gmail.com>
2017-07-27 17:55:30 -0700

    Update Windows build

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,8 @@
 # Build against imagemagick static website.
 IM_VERSION <- commandArgs(TRUE)
-if(!file.exists("../windows/imagemagick6-6.9.5-4/include/ImageMagick-6/Magick++.h")){
+if(!file.exists("../windows/imagemagick6-6.9.9-3/include/ImageMagick-6/Magick++.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/imagemagick6/archive/v6.9.5-4.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/imagemagick6/archive/v6.9.9-3.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
