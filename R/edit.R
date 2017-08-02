fe3c6fed132fb6e373872742a4b39792f69fd46f
Jeroen <jeroenooms@gmail.com>
2017-08-02 21:04:40 +0200

    Only check for svg for file paths

--- R/edit.R
+++ R/edit.R
@@ -61,7 +61,7 @@ image_read <- function(path, density = NULL, depth = NULL){
   } else {
     stop("path must be URL, filename or raw vector")
   }
-  if(!isTRUE(magick_config()$rsvg)){
+  if(is.character(path) && !isTRUE(magick_config()$rsvg)){
     if(any(grepl("\\.svg$", tolower(path))) || any(grepl("svg|mvg", tolower(image_info(image)$format)))){
       warning("ImageMagick was built without librsvg which causes poor qualty of SVG rendering.
 For better results, rebuild ImageMagick --with-librsvg or use the 'rsvg' package in R.", call. = FALSE)
