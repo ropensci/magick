10e52b235d9429705a4e96390d7d01f7c15a1d34
Jeroen <jeroenooms@gmail.com>
2017-11-27 22:04:36 +0100

    Improve tests for font families

--- tests/fontfamily.R
+++ tests/fontfamily.R
@@ -1,28 +1,26 @@
 # Check ?plotmath for symbol math examples
 
-img <- image_graph(height = 400, pointsize = 20, res = 100)
+img <- image_graph(width = 600, height = 400, pointsize = 20, res = 100)
+#png("families.png", width = 600, height = 400, pointsize = 20, res = 100)
+
 
 graphics::plot.new()
-graphics::par(mar = c(0,1,3,0))
-graphics::plot.window(xlim = c(0, 10), ylim = c(0, 8), xaxs = "i", yaxs = "i")
+graphics::par(mar = c(0,0,3,0))
+graphics::plot.window(xlim = c(0, 20), ylim = c(-.5, 8), xaxs = "i", yaxs = "i")
 
 title(expression(Gamma %prop% sum(x[alpha], i==1, n) * sqrt(mu)), expression(hat(x)))
 
 
+# Standard families as supported by other devices
+text(0.95, 7, "abcdefg  - Sans (Arial)", pos = 4, family = "sans")
+text(0.95, 6, "abcdefg - Serif (Times)", pos = 4, family = "serif")
+text(0.95, 5, "abcdefg - Monospace (Courier New)", pos = 4, family = "mono")
+text(0.95, 4, "abcdefg - Symbol Face", pos = 4, font = 5)
 
-# Sans fonts
-text(0.95, 1, "abcdefg  - Sans (Arial)", pos = 4, family = "Sans Serif")
+# Extra fonts implemented in magick
 text(0.95, 2, "abcdefg  - Comic Sans", pos = 4, family = "Comic Sans")
+text(0.95, 1, "abcdefg - Georgia Serif", pos = 4, family = "Georgia")
+text(0.95, 0, "abcdefg - Courier", pos = 4, family = "Courier")
 
-#Serif fonts
-text(0.95, 3, "abcdefg - Serif (Times)", pos = 4, family = "Serif")
-text(0.95, 4, "abcdefg - Georgia Serif", pos = 4, family = "Georgia")
-
-#Monospace
-text(0.95, 5, "abcdefg - Monospace (Courier New)", pos = 4, family = "Monospace")
-text(0.95, 6, "abcdefg - Courier", pos = 4, family = "Courier")
-
-#SYMBOL (both methods)
-text(0.95, 7, "abcdefg - Symbol Face", pos = 4, font = 5)
 
 dev.off()
