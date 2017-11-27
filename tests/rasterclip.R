d9dead087670340a766030a500c54290d8dac8c0
Jeroen <jeroenooms@gmail.com>
2017-11-27 23:45:25 +0100

    Update raster example

--- tests/rasterclip.R
+++ tests/rasterclip.R
@@ -1,15 +1,20 @@
 library(magick)
 # Using standard device
 frink <- image_read("https://jeroen.github.io/images/frink.png")
-plot(1, xlim = c(0, 100), ylim = c(0, 200))
-plot(as.raster(frink), add = T)
 
-# Using magick
-dev <- image_graph(800, 600)
-plot(1, xlim = c(0, 100), ylim = c(0, 200))
+png("cairo.png", 800, 600, res = 72)
+plot(1, xlim = c(0, 100), ylim = c(0, 200), main = "cairo")
 plot(as.raster(frink), add = T)
 dev.off()
-dev
+cairo <- image_read("cairo.png")
+unlink("cairo.png")
+
+# Using magick
+mgk <- image_graph(800, 600, res = 72)
+plot(1, xlim = c(0, 100), ylim = c(0, 200), main = "magick")
+plot(as.raster(frink), add = T)
+dev.off()
+c(cairo, mgk)
 
 # Stretchy example
 dev <- image_graph(800, 600, res = 96)
