b59bd9d024a3a3a47925b6cf6ade05eee4ee14da
Jeroen Ooms <jeroenooms@gmail.com>
2017-11-27 18:33:32 +0000

    Add another symbol test

--- tests/fontfamily.R
+++ tests/fontfamily.R
@@ -2,8 +2,8 @@ img <- image_graph(height = 400, pointsize = 20, res = 120)
 
 
 # Initialize plot & print text
-par(mar = c(0,0,0,0)) 
-plot(c(0,1.5), axes = FALSE, main = "", xlab = "", ylab = "", col = "white")
+par(mar = c(0,0,0,0))
+plot(c(0,1.6), axes = FALSE, main = "", xlab = "", ylab = "", col = "white")
 
 
 # Sans fonts
@@ -18,9 +18,8 @@ text(0.95, 0.7, "abcdefg - Georgia Serif", pos = 4, family = "Georgia")
 text(0.95, 0.9, "abcdefg - Monospace (Courier New)", pos = 4, family = "Monospace")
 text(0.95, 1.1, "abcdefg - Courier", pos = 4, family = "Courier")
 
-#Other
-text(0.95, 1.3, "abcdefg - Symbol", pos = 4, family = "Symbol")
+#SYMBOL (both methods)
+text(0.95, 1.3, "abcdefg - Symbol Family", pos = 4, family = "Symbol")
+text(0.95, 1.5, "ABCDEF - Symbol Face", pos = 4, font = 5)
 
-
-
-dev.off() 
+dev.off()
