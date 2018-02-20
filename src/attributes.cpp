b9f97c3bb5133078809f1e403e17a02a7b7b3e9e
Jeroen <jeroenooms@gmail.com>
2018-02-20 15:45:00 +0100

    Add density to info

--- src/attributes.cpp
+++ src/attributes.cpp
@@ -170,6 +170,7 @@ Rcpp::DataFrame magick_image_info( XPtrImage input){
   Rcpp::IntegerVector height(len);
   Rcpp::LogicalVector matte(len);
   Rcpp::IntegerVector filesize(len);
+  Rcpp::CharacterVector density(len);
   for(int i = 0; i < len; i++){
     Frame frame = input->at(i);
     colorspace[i] = Option(MagickCore::MagickColorspaceOptions, frame.colorSpace());
@@ -179,6 +180,7 @@ Rcpp::DataFrame magick_image_info( XPtrImage input){
     height[i] = geom.height();
     matte[i] = frame.hasMatte();
     filesize[i] = frame.fileSize();
+    density[i] = std::string(frame.density());
   }
   return Rcpp::DataFrame::create(
     Rcpp::_["format"] = format,
@@ -187,6 +189,7 @@ Rcpp::DataFrame magick_image_info( XPtrImage input){
     Rcpp::_["colorspace"] = colorspace,
     Rcpp::_["matte"] = matte,
     Rcpp::_["filesize"] = filesize,
+    Rcpp::_["density"] = density,
     Rcpp::_["stringsAsFactors"] = false
   );
 }
