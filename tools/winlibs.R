e9978e186c2eb31ef6b8b5f7ed1a8c903c6f1bec
Jeroen <jeroenooms@gmail.com>
2018-02-26 15:01:49 +0100

    Use curl to download because win builder SSL is broken right now

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -3,7 +3,7 @@ VERSION <- commandArgs(TRUE)
 IM <- substr(VERSION, 1,1)
 if(!file.exists(sprintf("../windows/imagemagick%s-%s/include/ImageMagick-%s/Magick++.h", IM, VERSION, IM))){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/imagemagick%s/archive/v%s.zip", IM, VERSION), "lib.zip", quiet = TRUE)
+  curl::curl_download(sprintf("https://github.com/rwinlib/imagemagick%s/archive/v%s.zip", IM, VERSION), "lib.zip")
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
