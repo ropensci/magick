3d1ed7fc6456cae565d446b22f394c87436ca28c
Jeroen <jeroenooms@gmail.com>
2018-05-31 19:01:56 +0200

    Tweak documentation

--- R/threshold.R
+++ R/threshold.R
@@ -3,6 +3,13 @@
 #' Thresholding an image can be used for simple and straightforward image segmentation.
 #' The function \code{image_threshold} allows to do black thresholding, white thresholding
 #' as well as local adaptive thresholding.
+#'
+#'  - `type = black`: Forces all pixels below the threshold into black while leaving all pixels
+#' at or above the threshold unchanged
+#'  - `type = white`: Forces all pixels above the threshold into white while leaving all pixels
+#' at or below the threshold unchanged
+#'  - `type = lat`: Local Adaptive Thresholding. Looks in a box (width x height) around the
+#' pixel neighborhood if the pixel value is bigger than the average minus an offset
 #' @export
 #' @name image_threshold
 #' @rdname image_threshold
@@ -10,12 +17,7 @@
 #' @param type type of thresholding, either one of lat, black or white (see details below)
 #' @param threshold pixel intensity threshold percentage for black or white thresholding
 #' @param geometry pixel window plus offset for LAT algorithm
-#' @details
-#'  - type = black: Forces all pixels below the threshold into black while leaving all pixels at or above the threshold unchanged
-#'  - type = white: Forces all pixels above the threshold into white while leaving all pixels at or below the threshold unchanged
-#'  - type = lat: Local Adaptive Thresholding. Looks in a box (width x height) around the pixel neighborhood if the pixel value is bigger than the average minus an offset
 #' @examples
-#' logo <- image_read("logo:")
 #' logo <- image_convert(logo, colorspace = "Gray")
 #' image_threshold(logo, type = "lat", geometry = "5x5+10")
 #' image_threshold(logo, type = "black", threshold = "50%")
