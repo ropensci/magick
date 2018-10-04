d235a3ff93a30cd2312bdc4be2a7f0760fc03b9f
Jeroen Ooms <jeroenooms@gmail.com>
2018-10-05 01:13:15 +0200

    Split manual page because examples take too long

---
+++ R/fx.R
@@ -0,0 +1,29 @@
+#' Image FX
+#'
+#' Apply a custom an [fx expression](https://www.imagemagick.org/script/fx.php) to the image.
+#'
+#' @export
+#' @inheritParams thresholding
+#' @rdname fx
+#' @name fx
+#' @family image
+#' @param expression string with an [fx expression](https://www.imagemagick.org/script/fx.php)
+#' @examples
+#' # Show image_fx() expression
+#' img <- image_convert(logo, colorspace = "Gray")
+#' image_fx(img, expression = "pow(p, 0.5)")
+#' image_fx(img, expression = "random()")
+#'
+#' gradient_x <- image_convolve(img, kernel = "Prewitt")
+#' gradient_y <- image_convolve(img, kernel = "Prewitt:90")
+#' gradient <- c(image_fx(gradient_x, expression = "p^2"),
+#'                 image_fx(gradient_y, expression = "p^2"))
+#' gradient <- image_flatten(gradient, operator = "Plus")
+#' gradient <- image_fx(gradient, expression = "sqrt(p)")
+#' gradient
+image_fx <- function(image, expression = "p", channel = NULL){
+  assert_image(image)
+  expression <- as.character(expression)
+  channel <- as.character(channel)
+  magick_image_fx(image, expression, channel)
+}
