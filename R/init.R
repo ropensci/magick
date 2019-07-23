827de43573fc2ee6e1c8b03c0fbd61bf3aae8c26
Jeroen Ooms <jeroenooms@gmail.com>
2019-07-23 11:03:53 +0200

    Prefer the X11 font data to not conflict with fontconfig used by base cairo/gdtools, see issue #209

--- R/init.R
+++ R/init.R
@@ -21,10 +21,10 @@
 
   if(autobrewed()){
     fontdir <- normalizePath(file.path(lib, pkg, "etc/fonts"), mustWork = FALSE)
-    if(file.exists(fontdir)){
-      Sys.setenv(FONTCONFIG_PATH = fontdir)
-    } else if(file.exists("/opt/X11/lib/X11/fontconfig")){
+    if(file.exists("/opt/X11/lib/X11/fontconfig")){
       Sys.setenv(FONTCONFIG_PATH = "/opt/X11/lib/X11/fontconfig")
+    } else if(file.exists(fontdir)){
+      Sys.setenv(FONTCONFIG_PATH = fontdir)
     }
   } else if(is_mac()){
     # Workaround for R's built-in OpenMP conflicts
