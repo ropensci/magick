14a50c78d7e325a76eeacf163e1613e94c8fca8d
Jeroen Ooms <jeroenooms@gmail.com>
2020-10-12 21:56:16 +0200

    Unrevert y-scale in image_ggplot() (#277)

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -33,8 +33,7 @@ image_ggplot <- function(image, interpolate = FALSE) {
   ggplot2::ggplot(data.frame(x = 0, y = 0), ggplot2::aes_string('x', 'y')) +
     ggplot2::geom_blank() +
     ggplot2::theme_void() +
-    ggplot2::scale_y_reverse() +
     ggplot2::coord_fixed(expand = FALSE, xlim = c(0, info$width), ylim = c(0, info$height)) +
-    ggplot2::annotation_raster(image, 0, info$width, -info$height, 0, interpolate = interpolate) +
+    ggplot2::annotation_raster(image, 0, info$width, info$height, 0, interpolate = interpolate) +
     NULL
 }
