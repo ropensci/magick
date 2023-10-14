172945fd5b1e5c1b853dfa4c80e00422decce56f
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-14 14:49:54 +0200

    Bump version

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,11 +1,11 @@
 if(!file.exists("../windows/imagemagick/include/ImageMagick-6/Magick++.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.96/imagemagick-6.9.12.96-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.98/imagemagick-6.9.12.98-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.96/imagemagick-6.9.12.96-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.98/imagemagick-6.9.12.98-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.3") {
-    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.96/imagemagick-6.9.12.96-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.98/imagemagick-6.9.12.98-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/imagemagick6/archive/v6.9.12-96.tar.gz"
   }
