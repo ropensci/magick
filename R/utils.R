ff8a8060c467b9ae0e298842a62aa573bb656f43
Jeroen Ooms <jeroenooms@gmail.com>
2023-12-20 14:08:35 +0100

    Remove unused functions

--- R/utils.R
+++ R/utils.R
@@ -2,21 +2,6 @@
 #' @export
 magrittr::`%>%`
 
-read_path <- function(path){
-  image <- if(is.character(path)){
-    if(is_url(path)){
-      read_url(path)
-    } else {
-      read_file(path)
-    }
-  } else if(is.raw(path)){
-    path
-  } else {
-    stop("Parameter 'image' must be an image object, file path or raw vector with image data")
-  }
-  return(image)
-}
-
 is_url <- function(path){
   grepl("^https?://", path)
 }
@@ -57,17 +42,6 @@ download_url <- function(url){
   filename <- tempfile(fileext = extension)
   writeBin(req$content, filename)
   return(filename)
-}
-
-read_url <- function(path){
-  req <- curl::curl_fetch_memory(path)
-  if(req$status_code >= 400)
-    stop(sprintf("Failed to download %s (HTTP %d)", path, req$status))
-  return(req$content)
-}
-
-read_file <- function(path){
-  readBin(normalizePath(path, mustWork = TRUE), raw(), file.info(path)$size)
 }
 
 assert_image <- function(image){
