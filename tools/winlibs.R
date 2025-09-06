48892b7cf3766b300339a3e77e225c18063f79ea
Jeroen Ooms <jeroenooms@gmail.com>
2025-09-06 16:45:33 +0200

    Windows: update ImageMagick stack

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,11 +1,11 @@
 if(!file.exists("base.o") && !file.exists("../.libs/imagemagick/include/ImageMagick-6/Magick++.h")){
   unlink("../.libs", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.98/imagemagick-6.9.12.98-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.13.29/imagemagick-6.9.13.29-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.98/imagemagick-6.9.12.98-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.13.29/imagemagick-6.9.13.29-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.3") {
-    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.12.98/imagemagick-6.9.12.98-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/imagemagick-6.9.13.29/imagemagick-6.9.13.29-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/imagemagick6/archive/v6.9.12-96.tar.gz"
   }
@@ -16,4 +16,10 @@ if(!file.exists("base.o") && !file.exists("../.libs/imagemagick/include/ImageMag
   unlink(basename(url))
   setwd("../.libs")
   file.rename(list.files(), 'imagemagick')
+}
+
+# Hack hack hack for old bundle
+if(getRversion() >= "4.2.0" && getRversion() < "4.2.9"){
+  file.copy('imagemagick/lib/libz.a', 'imagemagick/lib/libbrotlidec.a')
+  file.copy('imagemagick/lib/libz.a', 'imagemagick/lib/libbrotlicommon.a')
 }
