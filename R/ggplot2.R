451f38de5325455c0d9d43255aad55aa6f491c1e
Joe Zhu <sha.joe.zhu@gmail.com>
2025-12-23 02:49:55 +0800

    Fix ggplot2 aes_string to aes for compatibility (#423)

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -30,7 +30,7 @@
 #' grid.raster(image)
 image_ggplot <- function(image, interpolate = FALSE) {
   info <- image_info(image)
-  ggplot2::ggplot(data.frame(x = 0, y = 0), ggplot2::aes_string('x', 'y')) +
+  ggplot2::ggplot(data.frame(x = 0, y = 0), ggplot2::aes(x = .data[['x']], y = .data[['y']])) +
     ggplot2::geom_blank() +
     ggplot2::theme_void() +
     ggplot2::coord_fixed(expand = FALSE, xlim = c(0, info$width), ylim = c(0, info$height)) +
